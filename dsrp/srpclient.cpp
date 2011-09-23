6107154c537edfa33437726079d2be7cfd0ef58c
draco <draco@allstar.pilsfree.czf>
2011-09-23 18:38:31 +0200

    licence

--- dsrp/srpclient.cpp
+++ dsrp/srpclient.cpp
@@ -1,3 +1,22 @@
+/* ===============================================================  @
+*  DragonSRP, C++ library implementing Secure Remote Protocol       *
+*  Copyright (C) 2011 Pavel Slechta                                 *
+*  <slechta@email.cz>                                               *
+*                                                                   *
+*  DragonSRP is free software; you can redistribute it and/or       *
+*  modify it under the terms of the GNU Lesser General Public       *
+*  License as published by the Free Software Foundation; either     *
+*  version 3 of the License, or (at your option) any later version. *
+*                                                                   *
+*  DragonSRP is distributed in the hope that it will be useful,     *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU *
+*  Lesser General Public License for more details.                  *
+*                                                                   *
+*  You should have received a copy of the GNU Lesser General Public *
+*  License along with DragonSRP.                                    *
+*  If not, see <http://www.gnu.org/licenses/>.                      *
+@  =============================================================== */
 
 
 #include "srpclient.hpp"
